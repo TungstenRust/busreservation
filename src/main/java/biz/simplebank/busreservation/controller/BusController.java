e22c845c571ef21fa671e97ec83a43e771959f3f
Shahin Safaroff <tungsten.rust@gmail.com>
2024-06-04 00:08:31 +0400

    Response model for addbus and getallbus is done

--- src/main/java/biz/simplebank/busreservation/controller/BusController.java
+++ src/main/java/biz/simplebank/busreservation/controller/BusController.java
@@ -1,8 +1,11 @@
 package biz.simplebank.busreservation.controller;
 
 import biz.simplebank.busreservation.entities.Bus;
+import biz.simplebank.busreservation.models.ResponseModel;
 import biz.simplebank.busreservation.services.BusService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,12 +16,12 @@ public class BusController {
     @Autowired
     private BusService busService;
     @PostMapping("/add")
-    public String addBus(@RequestBody Bus bus) {
-        busService.addBus(bus);
-        return "Bus Saved Successfully";
+    public ResponseModel<Bus> addBus(@RequestBody Bus bus) {
+        final Bus savedBus = busService.addBus(bus);
+        return new ResponseModel<>(HttpStatus.OK.value(), "Bus Saved", savedBus);
     }
     @GetMapping("/all")
-    public List<Bus> getAllBus() {
-       return busService.getAllBus();
+    public ResponseEntity<List<Bus>> getAllBus() {
+       return ResponseEntity.ok(busService.getAllBus());
     }
 }
