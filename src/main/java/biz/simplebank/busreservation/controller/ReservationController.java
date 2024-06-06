658ef95f5399a7e8fd551e4a3c9412b7763900c1
Shahin Safaroff <tungsten.rust@gmail.com>
2024-06-06 22:46:38 +0400

    Reservation Components are done

--- src/main/java/biz/simplebank/busreservation/controller/ReservationController.java
+++ src/main/java/biz/simplebank/busreservation/controller/ReservationController.java
@@ -1,4 +1,42 @@
 package biz.simplebank.busreservation.controller;
 
-public interface ReservationController {
+import biz.simplebank.busreservation.entities.Reservation;
+import biz.simplebank.busreservation.models.ResponseModel;
+import biz.simplebank.busreservation.services.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/reservation")
+public class ReservationController {
+    @Autowired
+    private ReservationService reservationService;
+
+    @PostMapping("/add")
+    public ResponseModel<Reservation>addReservation(@RequestBody Reservation reservation){
+        final Reservation result = reservationService.addReservation(reservation);
+        return new ResponseModel<>(HttpStatus.OK.value(), "Reservation saved", result);
+    }
+    @GetMapping("/all")
+    public ResponseEntity<List<Reservation>>getAllReservations(){
+        return ResponseEntity.ok(reservationService.getAllReservations());
+    }
+    @GetMapping("/query")
+    public ResponseEntity<List<Reservation>>getReservationsByScheduleAndDepartureDate(
+        @RequestParam Long scheduleId,
+        @RequestParam String departureDate
+        ){
+        return ResponseEntity.ok(reservationService.getReservationsByScheduleAndDepartureDate(scheduleId, departureDate));
+    }
+    @GetMapping("/all/{mobile}")
+    public ResponseEntity<List<Reservation>> getReservationsByMobile(
+            @PathVariable(name = "mobile") String mobile
+        )
+        {
+            return ResponseEntity.ok(reservationService.getReservationsByMobile(mobile));
+    }
 }
--- src/main/java/biz/simplebank/busreservation/services/impl/ReservationServiceImpl.java
+++ src/main/java/biz/simplebank/busreservation/services/impl/ReservationServiceImpl.java
@@ -1,11 +1,15 @@
 package biz.simplebank.busreservation.services.impl;
 
+import biz.simplebank.busreservation.entities.BusSchedule;
+import biz.simplebank.busreservation.entities.Customer;
 import biz.simplebank.busreservation.entities.Reservation;
+import biz.simplebank.busreservation.models.ReservationApiException;
 import biz.simplebank.busreservation.repos.BusScheduleRepository;
 import biz.simplebank.busreservation.repos.CustomerRepository;
 import biz.simplebank.busreservation.repos.ReservationRepository;
 import biz.simplebank.busreservation.services.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,4 +22,38 @@ public class ReservationServiceImpl implements ReservationService {
   @Autowired
     private BusScheduleRepository busScheduleRepository;
 
+  @Override
+  public Reservation addReservation(Reservation reservation) {
+    final Customer customer;
+    final boolean doesCustomerExist = customerRepository
+            .existsByMobileOrEmail(reservation.getCustomer().getMobile(), reservation.getCustomer().getEmail());
+    if (doesCustomerExist) {
+      customer = customerRepository
+              .findByMobileOrEmail(reservation.getCustomer().getMobile(), reservation.getCustomer().getEmail());
+    }
+    else{
+      customer = customerRepository.save(reservation.getCustomer());
+    }
+    reservation.setCustomer(customer);
+    return reservationRepository.save(reservation);
+  }
+  @Override
+  public List<Reservation> getAllReservations() {return reservationRepository.findAll();}
+
+  @Override
+  public List<Reservation> getReservationsByScheduleAndDepartureDate(Long scheduleId, String departureDate) {
+    final BusSchedule schedule = busScheduleRepository
+            .findById(scheduleId)
+            .orElseThrow(() -> new ReservationApiException(HttpStatus.BAD_REQUEST, "Schedule not found"));
+    return reservationRepository.findByBusScheduleAndDepartureDate(schedule, departureDate)
+            .orElseThrow(() -> new ReservationApiException(HttpStatus.BAD_REQUEST, "Reservation not found"));
+
+    }
+  @Override
+  public List<Reservation> getReservationsByMobile(String mobile) {
+    final Customer customer = customerRepository
+            .findByMobile(mobile)
+            .orElseThrow(()-> new ReservationApiException(HttpStatus.BAD_REQUEST, "No record found"));
+    return reservationRepository.findByCustomer(customer).orElseThrow(()-> new ReservationApiException(HttpStatus.BAD_REQUEST, "Reservation not found"));
+  }
 }
