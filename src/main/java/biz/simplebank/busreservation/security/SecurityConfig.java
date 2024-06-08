d993f227ab499df24bf837f45cac35ef7a0ad53d
Shahin Safaroff <tungsten.rust@gmail.com>
2024-06-08 14:59:12 +0400

    Security Filter Chain is modified

--- src/main/java/biz/simplebank/busreservation/security/SecurityConfig.java
+++ src/main/java/biz/simplebank/busreservation/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package biz.simplebank.busreservation.security;
 
+import biz.simplebank.busreservation.services.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,21 +10,37 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+    @Autowired
+    private JWTEntryPoint jwtentrypoint;
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+    @Autowired
+    private JwtAuthFilter jwtauthFilter;
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
                 .requestMatchers(HttpMethod.GET)
                                 .permitAll()
-                                .anyRequest()
+                                .requestMatchers("/api/auth/**")
+                                .permitAll()
+                                .requestMatchers(HttpMethod.POST,
+                                        "/api/bus/add",
+                                        "api/schedule/add",
+                                        "api/route/add")
                                 .authenticated()
-                )
-                .httpBasic(Customizer.withDefaults());
+                                .requestMatchers(HttpMethod.POST, "/api/reservation/add")
+                                .permitAll())
+                                .exceptionHandling(exception -> exception.authenticationEntryPoint(jwtentrypoint))
+                                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.addFilterBefore(jwtauthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
     @Bean
